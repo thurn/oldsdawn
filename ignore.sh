ca05c9f8e201278779cb6437475749120b50caed
Derek Thurn <derek@fake.email>
2021-09-19 23:58:00 -0700

    initial commit

---
+++ ignore.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+xattr -w com.dropbox.ignored 1 Library/
+xattr -w com.dropbox.ignored 1 Logs/
+xattr -w com.dropbox.ignored 1 obj/
+xattr -w com.dropbox.ignored 1 UserSettings/
+xattr -w com.dropbox.ignored 1 Temp/
+xattr -w com.dropbox.ignored 1 out/
